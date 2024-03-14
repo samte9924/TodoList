a81751521427e09c128a4fa1c67e6970281e0519
samte9924 <tedsam04@gmail.com>
2024-03-14 17:45:48 +0100

    updated base url

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -12,7 +12,7 @@ const AuthForm = () => {
       view="magic_link"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo="https://todo-list-gold-pi.vercel.app/auth/callback"
       appearance={{
         theme: "dark",
         button: {
